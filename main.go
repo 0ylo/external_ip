a27b3c9417417113a93ada87e28e66a3d5080fc5
Anton <96187255+0ylo@users.noreply.github.com>
2022-07-02 13:45:50 +0300

    ResponseWriter added

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func main() {
-	handleRequest()
-}
-
-func handleRequest() {
 	http.HandleFunc("/", ip)
 	http.ListenAndServe(":8080", nil)
 }
 
 func ip(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("IP adress:\n", req.RemoteAddr)
-
+	w.Write([]byte(req.RemoteAddr))
+	return
 }
